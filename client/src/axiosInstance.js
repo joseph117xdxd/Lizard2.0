c84827546e53dd0827c4d23a9ab3408b20db0e69
Aura Renata <perezugaldeaurarenata@gmail.com>
2024-03-26 13:27:00 -0600

    ruta server arreglada en front

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://lizard-project-server-1gktrtdg3.vercel.app', 
+  baseURL: 'https://lizard-project-server.vercel.app/', 
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'custom value' },
 });
 
 export default axiosInstance;
