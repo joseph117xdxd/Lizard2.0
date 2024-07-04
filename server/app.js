c3334064f32c976e8edf3203ef7badf23f70ba8e
LuisAntonioD <luisdeanda320@gmail.com>
2024-07-04 17:30:57 -0600

    feat(back):Agregar roles

--- server/app.js
+++ server/app.js
@@ -38,10 +38,13 @@ import authRoutes from './src/routes/auth.routes.js';
 import ofertaRoutes from './src/routes/oferta.routes.js'
 import profesorRoutes from './src/routes/profesor.routes.js'
 import userRoutes from './src/routes/user.routes.js'
+import roles from './src/routes/roles.routes.js'
+
 app.use('/api/admision', admisionRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/oferta',ofertaRoutes);
 app.use('/api/profesores',profesorRoutes);
 app.use('/api/users',userRoutes);
+app.use('/api/roles',roles);
 
 export default app;
